925d7ce92c1174e9ed2942e3e17109614e7c3875
Louis Quinnell <hello@ziorobo.co>
2020-01-18 21:17:45 +1100

    Optimise for smaller (tiled) window layouts

--- main.js
+++ main.js
@@ -256,8 +256,8 @@ function handleCommonWindowEvents(window) {
 
 const DEFAULT_WIDTH = 800;
 const DEFAULT_HEIGHT = 610;
-const MIN_WIDTH = 680;
-const MIN_HEIGHT = 550;
+const MIN_WIDTH = 640;
+const MIN_HEIGHT = 360;
 const BOUNDS_BUFFER = 100;
 
 function isVisible(window, bounds) {
